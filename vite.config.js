d01d89ffcf6ea835e643eb5e2975ee7e87433d9d
dilmithp <dilmithp@outlook.com>
2025-07-14 17:10:46 +0530

    vercel v2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
